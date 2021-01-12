cfe33178cc4209bf54b43d8529b057448f870794
ehv <ehv@iastate.edu>
2021-01-12 14:43:21 -0600

    please work

--- components/rsvp-stepper.js
+++ components/rsvp-stepper.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import bw from "../static/Bwcute.jpg";
+import bw from "../static/cute.jpg";
 import Pane from "./pane";
 
 const StyledRSVPStepper = styled.div`
