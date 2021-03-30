2dbc17fcf6d2df33313aa9a7b32d79db9f4cdd07
EthanVDub <ehv@iastate.edu>
2021-03-29 19:14:06 -0500

    Added filtering

---
+++ components/rsvpresultsbutton.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const RSVPResultsButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #333333;
+  color: #333333;
+  width: 20%;
+  padding: 0.5em 1em;
+  font-size: 1em;
+  cursor: pointer;
+  max-height: 50px;
+  font-family: brandon-grotesque, "Brandon Grotesque", Futura, sans-serif;
+  :hover {
+    background: #333333;
+    color: white;
+  }
+
+  @media (max-width: 1200px) {
+    margin-bottom: 5%;
+    padding: 0.5em 0.5em;
+  }
+`;
+export default RSVPResultsButton;
--- pages/rsvpresults.js
+++ pages/rsvpresults.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 import Head from "next/head";
-import RSVPButton from "../components/rsvp-button";
+import RSVPResultsButton from "../components/rsvpresultsbutton";
 
 import { useRouter } from 'next/router';
 import { useState } from "react";
+
+
+const StyledIntro = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin: 2% 5%;
+`;
 
 const StyledRsvpResults = styled.div`
     display: flex;
@@ -29,23 +37,56 @@ const StyledResult = styled.div`
 
 const Rsvpresults = () => {
     const [data, setData] = useState([]);
+    const [filter, setFilter] = useState([]);
+    const [filterTypes, setFilterTypes] = useState([false, false, false, false]); //yes, no, kid, dietary
 
     const getResults = async () => {
       const res = await fetch("/api/results-api");
       const json = await res.json();
-      setData(json);
+      setData(json?.results);
+      setFilter(json?.results);
+    }
+
+    const filterResults = (yes, no, kid, dietary) => {
+      let temp = [...data]
+
+      if(yes) {
+       temp = temp.filter((res) => {
+         return res.response == "yes"
+       }) 
+      }
+
+      if(no) {
+       temp = temp.filter((res) => {
+         return res.response == "no"
+       }) 
+      }
+
+      if(kid) {
+       temp = temp.filter((res) => {
+         return res.kid == true
+       }) 
+      }
+
+      if(dietary) {
+       temp = temp.filter((res) => {
+         return res.dietary != ""
+       }) 
+      }
+
+      setFilter(temp);
     }
 
     let form = [];
-    
-    for (let i = 0; i < data.results?.length; i++) {
-        let firstname = data.results[i].firstname
-        let lastname = data.results[i].lastname
-        let response = data.results[i].response
-        let kid = data.results[i].kid
-        let dietary = data.results[i].dietary
-        let email = data.results[i].email
-        let party = data.results[i].partysize
+    for (let i = 0; i < filter?.length; i++) {
+        
+        let firstname = filter[i].firstname
+        let lastname = filter[i].lastname
+        let response = filter[i].response
+        let kid = filter[i].kid
+        let dietary = filter[i].dietary
+        let email = filter[i].email
+        let party = filter[i].partysize
 
       form.push(
           <StyledResult key={i}>
@@ -65,10 +106,56 @@ const Rsvpresults = () => {
             <link rel="icon" href="../static/ring.ico" />
             </Head>
             
-            <h1>Number of Responses: {data.results?.length || 0}</h1>
-            <RSVPButton onClick={() => {
-              getResults()
-            }}> Get Results </RSVPButton>
+            <StyledIntro>
+              <h1>Number of Responses: {filter?.length || 0}</h1>
+              <RSVPResultsButton onClick={() => {
+                getResults()
+              }}> Get Results </RSVPResultsButton>
+              <label for="yesfilter">Responded yes:</label>
+              <input
+                type="checkbox"
+                id="yesfilter"
+                name="yesfilter"
+                autoComplete="off"
+                onChange={() => { 
+                  filterResults(!filterTypes[0], filterTypes[1], filterTypes[2], filterTypes[3])
+                  setFilterTypes([!filterTypes[0], filterTypes[1], filterTypes[2], filterTypes[3]])
+                }}
+              />
+              <label for="nofilter">Responded no:</label>
+              <input
+                type="checkbox"
+                id="nofilter"
+                name="nofilter"
+                autoComplete="off"
+                onChange={() => { 
+                  filterResults(filterTypes[0], !filterTypes[1], filterTypes[2], filterTypes[3])
+                  setFilterTypes([filterTypes[0], !filterTypes[1], filterTypes[2], filterTypes[3]])
+                }}
+              />
+              <label for="kidfilter">Is a kid:</label>
+              <input
+                type="checkbox"
+                id="kidfilter"
+                name="kidfilter"
+                autoComplete="off"
+                onChange={() => { 
+                  filterResults(filterTypes[0], filterTypes[1], !filterTypes[2], filterTypes[3])
+                  setFilterTypes([filterTypes[0], filterTypes[1], !filterTypes[2], filterTypes[3]])
+                }}
+              />
+              <label for="dietary">Has dietary restrictions:</label>
+              <input
+                type="checkbox"
+                id="dietary"
+                name="dietary"
+                autoComplete="off"
+                onChange={() => { 
+                  filterResults(filterTypes[0], filterTypes[1], filterTypes[2], !filterTypes[3])
+                  setFilterTypes([filterTypes[0], filterTypes[1], filterTypes[2], !filterTypes[3]])
+                }}
+              />
+            </StyledIntro>
             <StyledRsvpResults>
               
               <style jsx global>{`
